b36c6e7e9a8429653d8e2268ff50ace7d44984b4
Xiaosa Liu <liu.changsen@qq.com>
2019-03-18 12:18:56 +0800

    刚刚忘了add  同上

--- routes/signup.js
+++ routes/signup.js
@@ -12,5 +12,55 @@ router.get('/signup', async(ctx, next) => {
         session: ctx.session,
     })
 })
+// post 注册
+router.post('/signup', async(ctx, next) => {
+    //console.log(ctx.request.body)
+    let user = {
+        name: ctx.request.body.name,
+        pass: ctx.request.body.password,
+        repeatpass: ctx.request.body.repeatpass,
+        avator: ctx.request.body.avator
+    }
+    await userModel.findDataByName(user.name)
+        .then(async (result) => {
+            console.log('findDataByName:',result)
+            if (result.length) {
+                try {
+                    throw Error('用户已经存在')
+                } catch (error) {
+                    //处理err
+                    console.log(error)
+                }
+                // 用户存在
+                ctx.body = {
+                    data: 1
+                };;
+                
+            } else if (user.pass !== user.repeatpass || user.pass === '') {
+                ctx.body = {
+                    data: 2,
+                    message:'两次输入的密码不一样,请确认密码'
+                };
+            } else {
+                // ctx.session.user=ctx.request.body.name   
+                let base64Data = user.avator.replace(/^data:image\/\w+;base64,/, "");
+                let dataBuffer = new Buffer(base64Data, 'base64');
+                let getName = Number(Math.random().toString().substr(3)).toString(36) + Date.now()
+                await fs.writeFile('./public/images/' + getName + '.png', dataBuffer, err => { 
+                    if (err) throw err;
+                    console.log('头像上传成功') 
+                });            
+                await userModel.insertData([user.name, md5(user.pass), getName, moment().format('YYYY-MM-DD HH:mm:ss')])
+                    .then(res=>{
+                        console.log('注册成功',res)
+                        //注册成功
+                        ctx.body = {
+                            data: 3,
+                            message:'注册成功'
+                        };
+                    })
+            }
+        })
+})
     
 module.exports = router
