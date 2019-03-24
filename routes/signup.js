7b6ce6e5f3ba04eaf4befd5f2a1faea3de7cc262
Xiaosa Liu <liu.changsen@qq.com>
2019-03-24 17:06:54 +0800

    写了两个接口

--- routes/signup.js
+++ routes/signup.js
@@ -5,16 +5,15 @@ const checkNotLogin = require('../middlewares/check.js').checkNotLogin
 const checkLogin = require('../middlewares/check.js').checkLogin
 const moment = require('moment');
 const fs = require('fs')
-const experts = require('../controller/c-expert.js');
 // 注册页面
-router.get('/signup', async(ctx, next) => {
+router.get('/signup', async (ctx, next) => {
     await checkNotLogin(ctx)
     await ctx.render('signup', {
         session: ctx.session,
     })
 })
 // post 管理员注册
-router.post('/signup', async(ctx, next) => {
+router.post('/signup', async (ctx, next) => {
     //console.log(ctx.request.body)
     let user = {
         name: ctx.request.body.name,
@@ -24,7 +23,7 @@ router.post('/signup', async(ctx, next) => {
     }
     await userModel.findDataByName(user.name)
         .then(async (result) => {
-            console.log('findDataByName:',result)
+            console.log('findDataByName:', result)
             if (result.length) {
                 try {
                     throw Error('用户已经存在')
@@ -36,36 +35,36 @@ router.post('/signup', async(ctx, next) => {
                 ctx.body = {
                     data: 1
                 };;
-                
+
             } else if (user.pass !== user.repeatpass || user.pass === '') {
                 ctx.body = {
                     data: 2,
-                    message:'两次输入的密码不一样,请确认密码'
+                    message: '两次输入的密码不一样,请确认密码'
                 };
             } else {
                 // ctx.session.user=ctx.request.body.name   
                 let base64Data = user.avator.replace(/^data:image\/\w+;base64,/, "");
                 let dataBuffer = new Buffer(base64Data, 'base64');
                 let getName = Number(Math.random().toString().substr(3)).toString(36) + Date.now()
-                await fs.writeFile('./public/images/' + getName + '.png', dataBuffer, err => { 
+                await fs.writeFile('./public/images/' + getName + '.png', dataBuffer, err => {
                     if (err) throw err;
-                    console.log('头像上传成功') 
-                });            
+                    console.log('头像上传成功')
+                });
                 await userModel.insertData([user.name, md5(user.pass), getName, moment().format('YYYY-MM-DD HH:mm:ss')])
-                    .then(res=>{
-                        console.log('注册成功',res)
+                    .then(res => {
+                        console.log('注册成功', res)
                         //注册成功
                         ctx.body = {
                             data: 3,
-                            message:'注册成功'
+                            message: '注册成功'
                         };
                     })
             }
         })
 })
 // post 专家注册
-router.post('/expSignup', async(ctx, next) => {
-    let { expert_name,password,repeatpass,expert_class,expert_info,province_ID } = ctx.request.body
+router.post('/expSignup', async (ctx, next) => {
+    let { expert_name, password, repeatpass, expert_class, expert_info, province_ID } = ctx.request.body
     await userModel.findExpertCountByName(expert_name)
         .then(async (result) => {
             console.log(result)
@@ -81,17 +80,71 @@ router.post('/expSignup', async(ctx, next) => {
                     message: '两次输入的密码不一致'
                 };
             } else {
-                    await userModel.insertExpert([expert_name, md5(password),expert_class ,expert_info, province_ID,moment().format('YYYY-MM-DD HH:mm:ss')])
-                        .then(res => {
-                            console.log('注册成功', res)
-                            //注册成功
-                            ctx.body = {
-                                code: 200,
-                                message: '注册成功'
-                            };
-                        })
+                await userModel.insertExpert([expert_name, md5(password), expert_class, expert_info, province_ID, moment().format('YYYY-MM-DD HH:mm:ss')])
+                    .then(res => {
+                        console.log('注册成功', res)
+                        //注册成功
+                        ctx.body = {
+                            code: 200,
+                            message: '注册成功'
+                        };
+                    })
             }
         })
 })
-    
+// post 创建学校
+router.post('/createSchool', async (ctx, next) => {
+    let { school_name, city_ID } = ctx.request.body
+    await userModel.schoolExist(school_name)
+        .then(async (result) => {
+            if (result[0].count >= 1) {
+                // 用户存在
+                ctx.body = {
+                    code: 500,
+                    message: '已存在学校'
+                };
+            } else {
+                await userModel.createSchool([school_name, city_ID, moment().format('YYYY-MM-DD HH:mm:ss')])
+                    .then(res => {
+                        console.log('注册成功', res)
+                        ctx.body = {
+                            code: 200,
+                            message: '注册成功'
+                        };
+                    })
+            }
+        })
+})
+
+router.get('/getProvince', async (ctx, next) => {
+    // await checkNotLogin(ctx)
+    await userModel.returnProvinceList()
+        .then(async (result) => {
+            ctx.body = {
+                code: 200,
+                data: result
+            };
+        })
+})
+
+router.get('/getCityList', async (ctx, next) => {
+    // await checkNotLogin(ctx)
+    let { province_ID } = ctx.query;
+    if (province_ID) {
+        await userModel.returnCityList(province_ID)
+            .then(async (result) => {
+                ctx.body = {
+                    code: 200,
+                    data: result
+                };
+            })
+    }else {
+        ctx.body = {
+            code: 500,
+            message:'请输入查询参数'
+        };
+    }
+})
+
+
 module.exports = router
